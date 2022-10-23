f04c0f60dd1ae8bfb323f463584de6d6f4809dc9
Oussama El Arbaoui <elarbaouioussama@gmail.com>
2022-10-23 20:33:44 +0900

    Add the Product component

---
+++ components/Product.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+export function Product(props) {
+  return (
+    <ProductContainer>
+      <ProductImage src={props.productImage} />
+      <ProductName>{props.productName}</ProductName>
+      <ProductPrice>{props.productPrice}</ProductPrice>
+      <ProductDescription>
+        {props.children}
+      </ProductDescription>
+    </ProductContainer>
+  )
+}
+
+const ProductContainer = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 170px 1fr 5fr;
+  background-color: #fff;
+  padding: 24px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px #a1a1a1;
+  width: 300px;
+  height: 380px;
+  margin: 16px;
+  color: #424242;
+`
+const ProductImage = styled.img`
+  grid-column: 1 / 3;
+  max-height: 200px;
+  margin: auto;
+  max-width: 100%;
+`
+const ProductName = styled.div`
+  color: #6b0b3d;
+  align-self: center;
+  margin-top: 10px;
+`
+
+const ProductPrice = styled.div`
+  color: #5e35b1;
+  align-self: center;
+  font-weight: bold;
+  justify-self: end;
+`
+
+const ProductDescription = styled.p`
+  grid-column: 1 / 3;
+  overflow: scroll;
+`
+
+// components
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { HomeHeader } from '../components/Header/Header'
+import { Product } from '../components/Product'
+import styled from '@emotion/styled'
 
 const Home: NextPage = () => {
   return (
@@ -11,8 +13,44 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeHeader />
+      <ProductsContainer>
+        <Product
+          productPrice="$10"
+          productName="Kit-Kat"
+          productImage="https://picsum.photos/300/200?random=1"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac
+          sagittis urna. Nulla eu orci placerat, congue magna eu, egestas purus.
+          Quisque ornare mauris ut blandit lobortis. Nulla at vulputate sem.
+          Vivamus ullamcorper sed enim eget tincidunt. Aliquam blandit condimentum
+          gravida. Etiam maximus metus vel euismod tincidunt.
+        </Product>
+        <Product
+          productPrice="$12"
+          productName="Coca-Cola"
+          productImage="https://picsum.photos/300/200?random=2"
+
+        >
+          This is coca cola
+        </Product>
+        <Product
+          productPrice="$100"
+          productName="CousCous"
+          productImage="https://picsum.photos/300/200?random=3"
+        />
+        <Product
+          productPrice="$99"
+          productName="Iphone"
+          productImage="https://picsum.photos/300/200?random=4"
+        />
+      </ProductsContainer>
     </div>
   )
 }
+
+const ProductsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
 
 export default Home
