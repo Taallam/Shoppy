272e15f7ca262d472a8e42a425890ec83ac0a6bb
Abderrahmane bouallaga <codeabdo61@gmail.com>
2022-11-16 15:00:25 +0100

    some style

--- components/Details.tsx
+++ components/Details.tsx
@@ -4,9 +4,9 @@ import styled from "@emotion/styled";
 export function Details(props) {
   return (
     <ProductContainer>
-      <ProductImageOne src={props.productImage} />
-      <ProductImageTow src={props.productImage}/>
-      <ProductImageTree src={props.productImage}/>
+      <ProductImageOne><Img src={props.productImage} /></ProductImageOne>
+      <ProductImageTow><Img src={props.productImage}/></ProductImageTow>
+      <ProductImageTree><Img src={props.productImage}/></ProductImageTree>
       <ProductName>{props.productName}</ProductName>
       <ProductPrice>{props.productPrice}</ProductPrice>
       <ProductDescription>{props.children}</ProductDescription>
@@ -14,36 +14,45 @@ export function Details(props) {
   );
 }
 
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  background-image: cover;
+  border-radius: 10px;
+`
 const ProductContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(6, 1fr);
   gap: 10px;
-  background-color: #434343;
   max-width: 80%;
   margin: 40px auto;
 `;
-const ProductImageOne = styled.img`
+const ProductImageOne = styled.div`
   grid-area: 1 / 1 / 4 / 4;
-  height: 400px;
-  background: cover;
-`;
-const ProductImageTow = styled.img`
-  grid-area: 1 / 4 / 2 / 5;
-  height: 170px;
+  max-width: 100%;
+  background-image: cover;
   
 `;
-const ProductImageTree = styled.img`
+const ProductImageTow = styled.div`
+  grid-area: 1 / 4 / 2 / 5;
+  max-width: 100%;
+  
+`;
+const ProductImageTree = styled.div`
   grid-area: 2 / 4 / 3 / 5;
-  height: 170px;
+  max-width: 100%;
 `;
 const ProductName = styled.h1`
   grid-area: 4 / 1 / 5 / 2;
-
+  margin: auto 0;
 `;
 
 const ProductPrice = styled.h1`
   grid-area: 4 / 2 / 5 / 3;
+  margin: auto 0;
+  color: #6B0B3D;
+  text-align: center;
 `;
 
 const ProductDescription = styled.p`
